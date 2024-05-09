aeb218ed2c1698ce99474d50db30c885440b4816
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-05-09 09:49:00 +0900

    now we can play video

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,8 +1,19 @@
 export default function Main(props) {
   const { data } = props;
+  const isImage = data.url.split(".").pop() ==='jpg';
+  
   return (
     <div className="imgContainer">
-      <img src={data.hdurl} alt={data.title || "bg-img"} className="bgImage" />;
+      
+      {isImage ? <img src={data.hdurl} />
+      :  <iframe width={960} height={540} src={data.url}
+      allowFullScreen 
+      >
+      </iframe>}
+      
+      {/* <img src={data.hdurl} alt={data.title || "bg-img"} 
+      className="bgImage" />; */}
+      
     </div>
   );
 }
